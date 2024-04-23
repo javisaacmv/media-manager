d32a9ec0ba030beb3432bd229ffe0c1be9340187
Javier Montano <javier.montano@imaginelearning.com>
2024-04-22 19:42:29 -0700

    endpoint

--- services/index.tsx
+++ services/index.tsx
@@ -2,8 +2,7 @@ import { useAuthStore } from "@/stores/authStore";
 import axios from "axios";
 
 const httpService = axios.create({
-  baseURL:
-    "https://media-manager-5cfkjhxrf-javiers-projects-5c30f34c.vercel.app/:4000/api",
+  baseURL: "https://media-manager-api.vercel.app/:4000/api",
   timeout: 3000,
   withCredentials: true,
   headers: {
